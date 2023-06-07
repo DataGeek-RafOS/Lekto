4490f9d3a6b08a3a9a3c9bddac8834b971902426
Rafael Rodrigues <rafael@datageek.com.br>
2023-06-07 11:19:04 -0300

    Bug: Duplicidade na distribuição de momentos (FUND I)

--- Diagrams/dbLektoRe/Scripts SQL/Procedure - Validação spuCreateLessonMoment.sql
+++ Diagrams/dbLektoRe/Scripts SQL/Procedure - Validação spuCreateLessonMoment.sql
@@ -1,6 +1,6 @@
 SET @in_txJobId := 'Geração Manual';
 
-
+## Queries
 
 SELECT @in_idNetwork := a.idNetwork
 	, @in_idSchool := a.idSchool
@@ -87,62 +87,6 @@ GROUP BY LeMo.idNetwork
 	  , LeMo.dtProcessed
 	  , LeMo.dtSchedule	
 	  , LeMo.coMomentStatus;
-		  
-
-
-		
-START TRANSACTION;
-
-DELETE MoGr
-FROM LessonMomentGroup MoGr
-	INNER JOIN LessonMomentActivity MoAc
-		   ON MoGr.idNetwork = MoAc.idNetwork
-		  AND MoGr.idSchool = MoAc.idSchool
-		  AND MoGr.coGrade = MoAc.coGrade
-		  AND MoGr.idSchoolYear = MoAc.idSchoolYear
-		  AND MoGr.idClass = MoAc.idClass
-		  AND MoGr.idLessonMomentActivity = MoAc.idLessonMomentActivity		
-	INNER JOIN LessonMoment Mome
-		   ON Mome.idNetwork		= MoAc.idNetwork
-		  AND Mome.idSchool			= MoAc.idSchool
-		  AND Mome.coGrade			= MoAc.coGrade
-		  AND Mome.idSchoolYear		= MoAc.idSchoolYear
-		  AND Mome.idClass			= MoAc.idClass
-		  AND Mome.idLessonMoment	= MoAc.idLessonMoment
-	INNER JOIN LessonTrackGroup TrGr
-		   ON TrGr.idLessonTrackGroup = Mome.idLessonTrackGroup		  
-WHERE MoAc.idLessonMoment IN (353,354,355,356);
-
-DELETE MoAc
-FROM LessonMomentActivity MoAc
-	INNER JOIN LessonMoment Mome
-		   ON Mome.idNetwork		= MoAc.idNetwork
-		  AND Mome.idSchool			= MoAc.idSchool
-		  AND Mome.coGrade			= MoAc.coGrade
-		  AND Mome.idSchoolYear		= MoAc.idSchoolYear
-		  AND Mome.idClass			= MoAc.idClass
-		  AND Mome.idLessonMoment	= MoAc.idLessonMoment
-WHERE MoAc.idLessonMoment IN (353,354,355,356);
-
-UPDATE LessonMoment
-SET coMomentStatus = 'AGEN', dtProcessed = NULL, txJobId = null 
-WHERE idLessonMoment IN (353,354,355,356);
-
-COMMIT;	
-
-
-CALL `spuCreateLessonMoment`
-(
-  @in_idNetwork 	 
-, @in_idSchool  	
-, @in_coGrade   	
-, @in_idSchoolYear  
-, @in_idClass       
-, @in_idProfessor   
-, @in_idLessonTrack 
-, @in_txJobId       
-);			
-
 
 SELECT @in_idNetwork := a.idNetwork
 	, @in_idSchool := a.idSchool
@@ -164,6 +108,8 @@ GROUP BY a.idNetwork
 	  , a.idProfessor
 	  , b.idLessonTrack;
 	  
+#________________________________________________________________________________________________________________________________________________________________$
+
 
 
 
@@ -192,13 +138,13 @@ FROM LessonMoment LeMo
 		  AND Grup.idSchoolYear = MoAc.idSchoolYear
 		  AND Grup.idClass = MoAc.idClass
 		  AND Grup.idLessonMomentActivity = MoAc.idLessonMomentActivity							 
-WHERE LeMo.idNetwork	 = @in_idNetwork
-AND   LeMo.idSchool		 = @in_idSchool
-AND   LeMo.coGrade		 = @in_coGrade
-AND   LeMo.idSchoolYear	 = @in_idSchoolYear
-AND   LeMo.idClass		 = @in_idClass
-AND   LeMo.idProfessor	 = @in_idProfessor
-AND   TrGr.idLessonTrack  = @in_idLessonTrack
+WHERE LeMo.idNetwork	 = 1
+AND   LeMo.idSchool		 = 324
+AND   LeMo.coGrade		 = 'F1A1'
+AND   LeMo.idSchoolYear	 = 1
+AND   LeMo.idClass		 = 10295
+AND   LeMo.idProfessor	 = 5903
+AND   TrGr.idLessonTrack  = 6
 GROUP BY LeMo.idLessonMoment 
 	  , LeAc.nuOrder
 	  , MoAc.idLessonMomentActivity 
@@ -206,77 +152,170 @@ GROUP BY LeMo.idLessonMoment
 
 
 
-
-
-/*
-
-
-SELECT LeMo.idLessonMoment AS Momento
-	, LeAc.nuOrder AS EtapaAula
-	, MoAc.idLessonMomentActivity AS MomentoGerado
-	, LeMo.coMomentStatus
-	, GROUP_CONCAT( Grup.idStudent ORDER BY Grup.idStudent) AS Alunos
+SELECT LeMo.idNetwork
+	, LeMo.idSchool
+	, LeMo.coGrade
+	, LeMo.idSchoolYear
+	, LeMo.idClass
+	, LeMo.idProfessor
+	, TrGr.idLessonTrack
+	, LeMo.idLessonMoment
+	, coMomentStatus
 FROM LessonMoment LeMo
 	INNER JOIN LessonTrackGroup TrGr
-		   ON TrGr.idLessonTrackGroup = LeMo.idLessonTrackGroup
-	LEFT JOIN LessonMomentActivity MoAc	
-		   ON MoAc.idNetwork		= LeMo.idNetwork
-		  AND MoAc.idSchool			= LeMo.idSchool
-		  AND MoAc.coGrade			= LeMo.coGrade
-		  AND MoAc.idSchoolYear		= LeMo.idSchoolYear
-		  AND MoAc.idClass			= LeMo.idClass
-		  AND MoAc.idLessonMoment	= LeMo.idLessonMoment
-	LEFT JOIN LessonActivity LeAc
-		   ON LeAc.idLessonActivity = MoAc.idLessonActivity
-		  AND LeAc.coGrade = MoAc.coGrade
-	LEFT JOIN LessonMomentGroup Grup
-		   ON Grup.idNetwork = MoAc.idNetwork
-		  AND Grup.idSchool = MoAc.idSchool
-		  AND Grup.coGrade = MoAc.coGrade
-		  AND Grup.idSchoolYear = MoAc.idSchoolYear
-		  AND Grup.idClass = MoAc.idClass
-		  AND Grup.idLessonMomentActivity = MoAc.idLessonMomentActivity							 
+		   ON TrGr.idLessonTrackGroup = LeMo.idLessonTrackGroup 
 WHERE LeMo.idNetwork	 = 1
-AND   LeMo.idSchool		 = 308
-AND   LeMo.coGrade		 = 'F1A2'
+AND   LeMo.idSchool		 = 324
+AND   LeMo.coGrade		 = 'F1A1'
 AND   LeMo.idSchoolYear	 = 1
-AND   LeMo.idClass		 = 9728
-AND   LeMo.idProfessor	 = 4216
-AND   TrGr.idLessonTrack  = 14
-GROUP BY LeMo.idLessonMoment 
-	  , LeAc.nuOrder
-	  , MoAc.idLessonMomentActivity 
-	  , LeMo.coMomentStatus
-	  
-	  
+AND   LeMo.idClass		 = 10295
+AND   LeMo.idProfessor	 = 5903
+AND   TrGr.idLessonTrack  = 6   
+ORDER BY LeMo.idLessonMoment DESC		 
+
+
+
+
+		
+START TRANSACTION;
+
+DELETE Asse
+FROM LessonGroupAssessment Asse
+	INNER JOIN LessonMomentGroup MoGr
+		   ON MoGr.idLessonMomentGroup = Asse.idLessonMomentGroup
+		  AND MoGr.idLessonActivity = Asse.idLessonActivity
+		  AND MoGr.coGrade = Asse.coGrade
+	INNER JOIN LessonMomentActivity MoAc
+		   ON MoGr.idNetwork = MoAc.idNetwork
+		  AND MoGr.idSchool = MoAc.idSchool
+		  AND MoGr.coGrade = MoAc.coGrade
+		  AND MoGr.idSchoolYear = MoAc.idSchoolYear
+		  AND MoGr.idClass = MoAc.idClass
+		  AND MoGr.idLessonMomentActivity = MoAc.idLessonMomentActivity		
+	INNER JOIN LessonMoment Mome
+		   ON Mome.idNetwork		= MoAc.idNetwork
+		  AND Mome.idSchool			= MoAc.idSchool
+		  AND Mome.coGrade			= MoAc.coGrade
+		  AND Mome.idSchoolYear		= MoAc.idSchoolYear
+		  AND Mome.idClass			= MoAc.idClass
+		  AND Mome.idLessonMoment	= MoAc.idLessonMoment
+	INNER JOIN LessonTrackGroup TrGr
+		   ON TrGr.idLessonTrackGroup = Mome.idLessonTrackGroup		  
+WHERE MoAc.idLessonMoment IN (725,726,727,728);
+
+
+DELETE MoGr
+FROM LessonMomentGroup MoGr
+	INNER JOIN LessonMomentActivity MoAc
+		   ON MoGr.idNetwork = MoAc.idNetwork
+		  AND MoGr.idSchool = MoAc.idSchool
+		  AND MoGr.coGrade = MoAc.coGrade
+		  AND MoGr.idSchoolYear = MoAc.idSchoolYear
+		  AND MoGr.idClass = MoAc.idClass
+		  AND MoGr.idLessonMomentActivity = MoAc.idLessonMomentActivity		
+	INNER JOIN LessonMoment Mome
+		   ON Mome.idNetwork		= MoAc.idNetwork
+		  AND Mome.idSchool			= MoAc.idSchool
+		  AND Mome.coGrade			= MoAc.coGrade
+		  AND Mome.idSchoolYear		= MoAc.idSchoolYear
+		  AND Mome.idClass			= MoAc.idClass
+		  AND Mome.idLessonMoment	= MoAc.idLessonMoment
+	INNER JOIN LessonTrackGroup TrGr
+		   ON TrGr.idLessonTrackGroup = Mome.idLessonTrackGroup		  
+WHERE MoAc.idLessonMoment IN (725,726,727,728);
+
+DELETE MoAc
+FROM LessonMomentActivity MoAc
+	INNER JOIN LessonMoment Mome
+		   ON Mome.idNetwork		= MoAc.idNetwork
+		  AND Mome.idSchool			= MoAc.idSchool
+		  AND Mome.coGrade			= MoAc.coGrade
+		  AND Mome.idSchoolYear		= MoAc.idSchoolYear
+		  AND Mome.idClass			= MoAc.idClass
+		  AND Mome.idLessonMoment	= MoAc.idLessonMoment
+WHERE MoAc.idLessonMoment IN (725,726,727,728);
+
+UPDATE LessonMoment
+SET coMomentStatus = 'AGEN', dtProcessed = NULL, txJobId = null 
+WHERE idLessonMoment IN (725,726,727,728);
+
+COMMIT;	
+
 
 CALL `spuCreateLessonMoment`
 (
   1 	 
-, 308  	
-, 'F1A2'   	
+, 324  	
+, 'F1A1'   	
 , 1  
-, 9728       
-, 4216   
-, 14 
-, 'Geração Manual'       
-);				  
+, 10295       
+, 5903   
+, 6  
+, 'debug distribuição'    
+, 1   
+);			
+  
+  
+  
 
 
 
-SELECT * 
+
+SELECT TrGr.idLessonTrack     AS Trilha 
+	, LeMo.idLessonMoment 	AS Momento
+	, Less.idLesson 		AS Aula
+	, Less.txTitle 		AS TituloAula
+	, Step.nuOrder 		AS PassoOrdem
+	, Acti.nuOrder 		AS AtividadeOrdem 
+	, LeMo.coMomentStatus	AS StatusMomento
+	, GROUP_CONCAT( Grup.idStudent ORDER BY Grup.idStudent) AS Alunos
 FROM LessonMoment LeMo
-	INNER JOIN LessonMomentActivity MoAc	
-		   ON MoAc.idNetwork		= LeMo.idNetwork
-		  AND MoAc.idSchool			= LeMo.idSchool
-		  AND MoAc.coGrade			= LeMo.coGrade
-		  AND MoAc.idSchoolYear		= LeMo.idSchoolYear
-		  AND MoAc.idClass			= LeMo.idClass
-		  AND MoAc.idLessonMoment	= LeMo.idLessonMoment
-	INNER JOIN LessonActivity LeAc
-		   ON LeAc.idLessonActivity = MoAc.idLessonActivity	
+	INNER JOIN LessonTrackGroup TrGr
+		   ON TrGr.idLessonTrackGroup = LeMo.idLessonTrackGroup
+	INNER JOIN LessonComponent Cmpn
+		   ON Cmpn.idLesson 	= TrGr.idLesson
+		  AND Cmpn.coGrade  	= TrGr.coGrade
+		  AND Cmpn.coComponent 	= TrGr.coComponent
+	INNER JOIN Lesson Less
+		   ON Less.idLesson = Cmpn.idLesson
+		  AND Less.coGrade	= Cmpn.coGrade
 	INNER JOIN LessonStep Step
-		   ON Step.idLessonStep = LeAc.idLessonStep AND Step.coGrade = LeAc.coGrade
-WHERE LeMo.idLessonMoment = 353		   
-
-*/
+		   ON Step.idLesson = Less.idLesson
+		  AND Step.coGrade  = Less.coGrade
+	INNER JOIN LessonActivity Acti
+		   ON Acti.idLessonStep 	= Step.idLessonStep
+		  AND Acti.coGrade 		= Step.coGrade
+	LEFT JOIN  LessonMomentActivity MoAc	
+		   ON MoAc.idLessonActivity   = Acti.idLessonActivity  -- [ Vinculo
+		  AND MoAc.coGrade			= Acti.coGrade			-- [ de atividades de aulas
+		  ##  
+		  AND MoAc.idNetwork		= LeMo.idNetwork		-- [
+		  AND MoAc.idSchool			= LeMo.idSchool		--   [ Vinculo
+		  AND MoAc.coGrade			= LeMo.coGrade			--   [ por momento
+		  AND MoAc.idSchoolYear		= LeMo.idSchoolYear		-- 	[
+		  AND MoAc.idClass			= LeMo.idClass			-- 	[
+		  AND MoAc.idLessonMoment	= LeMo.idLessonMoment	-- [
+	LEFT JOIN LessonMomentGroup Grup
+		   ON Grup.idNetwork 		= MoAc.idNetwork
+		  AND Grup.idSchool 		= MoAc.idSchool
+		  AND Grup.coGrade 			= MoAc.coGrade
+		  AND Grup.idSchoolYear 		= MoAc.idSchoolYear
+		  AND Grup.idClass 			= MoAc.idClass
+		  AND Grup.idLessonActivity 	= MoAc.idLessonActivity
+		  AND Grup.idLessonMomentActivity = MoAc.idLessonMomentActivity							 
+WHERE LeMo.idNetwork	 = 1
+AND   LeMo.idSchool		 = 324
+AND   LeMo.coGrade		 = 'F1A1'
+AND   LeMo.idSchoolYear	 = 1
+AND   LeMo.idClass		 = 10295
+AND   LeMo.idProfessor	 = 5903
+AND   TrGr.idLessonTrack  = 6
+AND   MoAc.idLessonMomentActivity IS NOT NULL
+GROUP BY TrGr.idLessonTrack  
+	  , LeMo.idLessonMoment 	
+	  , Less.idLesson 		
+	  , Less.txTitle 		
+	  , Step.nuOrder 		
+	  , Acti.nuOrder 		
+	  , LeMo.coMomentStatus;	
+	  
